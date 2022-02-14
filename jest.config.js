fd3efe5c607f1080ce924302980afb40f6131451
Jakub Jastrzębski <jakub.jastrzebski@pm.me>
2022-02-14 20:26:50 +0100

    test: add config snapshots

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,14 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        // needed for inline snapshots https://github.com/facebook/jest/issues/6744#issuecomment-861722016
+        sourceMap: true,
+      },
+    },
+  },
   coverageThreshold: {
     global: {
       branches: 90,
